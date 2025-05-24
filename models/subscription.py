86235db9cd685be6272f45f7b5053eb65b0eec4f
Fahad <fullstackdeveloper404@gmail.com>
2025-05-24 21:03:54 +0530

    Completed adding attributes and methods to User Class and Subscription Class. Validation is pending for both

---
+++ models/subscription.py
@@ -0,0 +1,111 @@
+class Subscription:
+    def __init__(self, service_type, service_name, plan_type, active_status, subscription_price, billing_frequency, start_date, renewal_date, auto_renewal_status):
+        self.service_type = service_type
+        self.service_name = service_name
+        self.plan_type = plan_type
+        self.active_status = active_status
+        self.subscription_price = subscription_price
+        self.billing_frequency = billing_frequency
+        self.start_date = start_date
+        self.renewal_date = renewal_date
+        self.auto_renewal_status = auto_renewal_status
+        
+    @property
+    def service_type(self):
+        return self._service_type
+
+    @service_type.setter
+    def service_type(self, service_type):
+        if not service_type:
+            raise ValueError("Service type cannot be empty")
+        else:
+            self._service_type = service_type
+            
+    @property
+    def service_name(self):
+        return self._service_name
+    
+    @service_name.setter
+    def service_name(self, service_name):
+        if not service_name:
+            raise ValueError("Service name cannot be empty")
+        else:
+            self._service_name = service_name
+            
+    @property
+    def plan_type(self):
+        return self._plan_type
+    
+    @plan_type.setter
+    def plan_type(self, plan_type):
+        if not plan_type:
+            raise ValueError("Plan type cannot be empty")
+        else:
+            self._plan_type = plan_type
+    
+    @property
+    def active_status(self):
+        return self._active_status
+    
+    @active_status.setter
+    def active_status(self, active_status):
+        if not active_status:
+            raise ValueError("Active status cannot be empty")
+        else:
+            self._active_status = active_status
+            
+    @property
+    def subscription_price(self):
+        return self._subscription_price
+    
+    @subscription_price.setter
+    def subscription_price(self, subscription_price):
+        if subscription_price is None or isinstance(subscription_price, str):
+            raise ValueError("Subscription price must be a number")
+        else:
+            self._subscription_price = subscription_price
+            
+    @property
+    def billing_frequency(self):
+        return self._billing_frequency
+    
+    @billing_frequency.setter
+    def billing_frequency(self, billing_frequency):
+        if not billing_frequency:
+            raise ValueError("Billing frequency cannot be empty")
+        else:
+            self._billing_frequency = billing_frequency
+            
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if not start_date:
+            raise ValueError("Start date cannot be empty")
+        else:
+            self._start_date = start_date
+            
+    @property
+    def renewal_date(self):
+        return self._renewal_date
+    
+    @renewal_date.setter
+    def renewal_date(self, renewal_date):
+        if not renewal_date:
+            raise ValueError("Renewal date cannot be empty")
+        else:
+            self._renewal_date = renewal_date
+            
+    @property
+    def auto_renewal_status(self):
+        return self._auto_renewal_status
+    
+    @auto_renewal_status.setter
+    def auto_renewal_status(self, auto_renewal_status):
+        if not auto_renewal_status:
+            raise ValueError("Auto renewal status cannot be empty")
+        else:
+            self._auto_renewal_status = auto_renewal_status
+            
---
+++ models/user.py
@@ -0,0 +1,72 @@
+from subscription import Subscription
+from budget import Budget
+import re
+
+class User:
+    def __init__(self, username, email_id, password):
+        self.username = username
+        self.email_id = email_id
+        self.password = password
+        self._subscription_list = []
+        self._budget = None
+        
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        correct_pattern = r"^[A-Za-z]+$"
+        if not username:
+            raise ValueError("Username cannot be empty")
+        if re.match(correct_pattern, username):
+            self._username = username
+        else:
+            raise ValueError("Username can contain only alphabets")
+    @property
+    def email_id(self):
+        return self._email_id
+    
+    @email_id.setter
+    def email_id(self, email_id):
+        correct_pattern = r"^[A-Za-z0-9\.]+@[A-Za-z]+\.[A-Za-z]+"
+        if not email_id:
+            raise ValueError("Email ID cannot be empty")
+        if re.match(correct_pattern, email_id):
+            self._email_id = email_id
+        else:
+            raise ValueError("Invalid Email ID")
+            
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, password):
+        if not password:
+            raise ValueError("Password cannot be empty")
+        elif len(password) < 8:
+            raise ValueError('Password must have atleast 8 characters')
+        else:
+            self._password = password
+            
+    @property
+    def subscription_list(self):
+        return self._subscription_list
+
+    def add_subscription(self, sub):
+        if isinstance(sub, Subscription):
+            self._subscription_list.append(sub)
+        else:
+            raise ValueError("Invalid Subscription")
+            
+    @property
+    def budget(self):
+        return self._budget
+    
+    @budget.setter
+    def budget(self, budget):
+        if isinstance(budget, Budget):
+            self._budget = budget
+        else:
+            raise ValueError('Invalid budget')
